927460c58fa5b476bffba8451189df67013be73c
imdbtelevision <161870953+imdbtelevision@users.noreply.github.com>
2024-05-27 21:31:33 +0400

    Update config.py

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ PROTECT_CONTENT = True if os.environ.get('PROTECT_CONTENT', "False") == "True" e
 DISABLE_CHANNEL_BUTTON = os.environ.get("DISABLE_CHANNEL_BUTTON", None) == 'True'
 
 BOT_STATS_TEXT = "<b>BOT UPTIME</b>\n{uptime}"
-USER_REPLY_TEXT = "❌Don't send me messages directly Here you can send your request @IMDB_TELEVISION"
+USER_REPLY_TEXT = "❌Don't Send Me Messages Directly Here\n\nYou Can Send Your Request @IMDB_TELEVISION"
 
 ADMINS.append(OWNER_ID)
 ADMINS.append(1250450587)
